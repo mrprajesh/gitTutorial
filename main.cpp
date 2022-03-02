1cf8b60d0fc0bc5ed244fc7c478d96c4cc7313f5
Rajesh Pandian M <mrpraj@gmail.com>
2022-03-03 00:57:11 +0530

    Addded diff in dev

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int sumOf(int i, int h);
+int diffOf(int i, int h);
 int main(int argc, char* argv[]){
   std::cout << "Hello Dear Programmer! \n\n --cpp\n";
   std::cout<< "sum:" << sum(123+456) << '\n';
+  std::cout<< "dif:" << diffOf(123,456) << '\n';
   return 0;
 }
 
 int sumOf(int i, int h){
+  return i+h;
+}
+
+int diffOf(int i, int h){
   return i+h;
 }
