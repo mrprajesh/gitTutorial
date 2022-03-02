d42dfd808d4275000a0d477380bc6d85bed9fc3c
Rajesh Pandian M <mrpraj@gmail.com>
2022-03-03 00:49:12 +0530

    added main in main branch

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//~~~START:Thu, 03-Mar-2022, 00:48:02 IST
+//~~~Author:Rajesh Pandian M | mrprajesh.co.in
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char* argv[]){
+  std::cout << "Hello Dear Programmer! \n\n --cpp\n";
+
+  return 0;
+}
